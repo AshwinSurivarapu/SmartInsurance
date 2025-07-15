8c21127590cd115fa0e59cf709d88186256eba74
AshwinS12 <ashwin.vtalent@gmail.com>
2025-07-15 10:20:40 +0530

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign:'center',marginTop:'50px'}}>
+      <h1>Welcome to SmartInsurance Customer Portal!</h1>
+      <p>This is your starting point for building the UI</p>
+      <Button variant="contained"color="primary">
+        MUI button Test
+      </Button>
+     
     </div>
   );
 }
